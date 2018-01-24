d1c3db1508bb05c668f8a2766fd0d1a71293014a
Samuel Mast <smast@Samuels-MacBook-Pro.local>
2018-01-23 23:45:05 -0700

    figured out writing values for hp to html, but still needs corrections to the reset to play second game.  Also needs to make dead players red

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,32 +1,37 @@
 $(document).ready(function() {
 
-//Button Creation
+//Global Variables
 
+//objects
 	var obi = {
+		var: "obi",
 		name:"Obi-Wan Kenobi", 
 		attack: "20",
 		healthPoints:"175",
 		counterAttack:"20",
-		src:"https://upload.wikimedia.org/wikipedia/en/thumb/3/32/Ben_Kenobi.png/220px-Ben_Kenobi.png"
+		src:"https://www.hasbro.com/common/productimages/en_AU/80520fda50569047f523b6138dd58048/8057953D50569047F527CFD4A4CAF309.jpg"
 	}
 		
 	var luke = {
+		var: "luke",
 		name:"Luke Skywalker", 
 		attack: "40",
 		healthPoints:"125",
 		counterAttack:"10",
-		src:"https://pm1.narvii.com/6009/bb9e18d9f023151491f8b9b1ea65e352737877b8_hq.jpg"
+		src:"http://moziru.com/images/luke-skywalker-clipart-3.jpg"
 	}	
 		
 	var darth = {
+		var: "darth",
 		name:"Darth Vader", 
 		attack: "30",
 		healthPoints:"150",
 		counterAttack:"30",
-		src:"https://images.moviepilot.com/image/upload/c_fill,h_470,q_auto:good,w_620/darth-vader_6bda9114-star-wars-art-challenge-features-crazy-dark-side-redesigns-jpeg-228992.jpg"
+		src:"https://cdn.shopify.com/s/files/1/0732/4767/products/Darth_Vader_New_Hope_Star_Wars_SHFiguarts_Bandai_Tamashii_Nations_Woozy_Moo.jpg?v=1512413304"
 	}	
 
 	var jabba = {
+		var: "jabba",
 		name:"Jabba the Hut", 
 		attack: "10",
 		healthPoints:"200",
@@ -48,6 +53,8 @@ $(document).ready(function() {
 	var enemyCharacterHp = "";	
 	var userCharacterAttack = "";
 	var enemyCharacterCounter = "";	
+	var userCharacterVar = "";
+	var enemyCharacterVar = "";
 
 	var enemiesLeft = [];
 	var currentEnemy = [];
@@ -56,9 +63,7 @@ $(document).ready(function() {
 	var userCharacterPicked = false;
 	var enemyCharacterPicked = false;
 
-//functions
-
-//create button
+//Functions
 
 function createButton() {
 	for (var i = 0; i < characters.length; i++) {
@@ -68,66 +73,111 @@ function createButton() {
         characterBtn.attr("character-hp", characters[i].healthPoints);
         characterBtn.attr("character-attack", characters[i].attack);
         characterBtn.attr("character-counter", characters[i].counterAttack);
+        characterBtn.attr("character-var", characters[i].var);
         characterBtn.addClass("character");
 
 
 
-        characterBtn.html("<img src='" + characters[i].src + "' class='image'/><br>"); 
+        characterBtn.html("<img src='" + characters[i].src + "' class='image'/><br><div id='" + characters[i].var + "'>" + characters[i].healthPoints + "</div>"); 
 
         characterBtn.prepend(characters[i].name);
-
-        characterBtn.append(characters[i].healthPoints);
 
         $("#pickCharacter").append(characterBtn);
 
       }
 }
 
+	
+function reset() {
+	
+	characters = [obi, luke, darth, jabba];
+	userCharacter = [];
+	userCharacterName = "";
+	enemyCharacterName = "";
+	userCharacterAttack = "";
+	enemyCharacterAttack = "";
+	userCharacterHp = "";
+	enemyCharacterHp = "";	
+	userCharacterAttack = "";
+	enemyCharacterCounter = "";	
+	userCharacterVar = "";
+	enemyCharacterVar = "";
 
+	enemiesLeft = [];
+	currentEnemy = [];
+
+
+	userCharacterPicked = false;
+	enemyCharacterPicked = false;
+
+	$("#pickText").show();
+	$("#userCharacter").html("");
+	$("#enemiesLeft").html("");
+	$("#currentEnemy").html("");
+
+}
+
+//Start Here
+
+	//initial button create
 	createButton();
+
+
 
 // On Click events
 
-
+	
 	$(".character").on("click", function() {
 
-
+		//if you havent picked a character yet
 	   	if (userCharacterPicked === false) {
+
+	   		alert("working...");
 
 		    $("#pickText").hide();
 
 		    userCharacter = $(this).append(characters);
-		    console.log(userCharacter);
 		    $("#userCharacter").append(userCharacter);
 
 		    userCharacterName = $(this).attr("character-name");
 		    userCharacterHp = $(this).attr("character-hp");
 		    userCharacterAttack = $(this).attr("character-attack");
+		    userCharacterVar = $(this).attr("character-var");
+
 
 		    userCharacterPicked = true;
 
 		    $(".character").each(function () {
 		       	if ($(this).attr("character-name") != userCharacterName) {
 		        	$("#enemiesLeft").append(this);
+		        
 		        };
 
 		    });
 
+		//else if you havent picked an enemy yet
 	   	} else if (enemyCharacterPicked === false) {
 
 		   	currentEnemy = $(this).append(characters);
-		    console.log(currentEnemy);
 		    $("#currentEnemy").append(currentEnemy);
 
 		    enemyCharacterName = $(this).attr("character-name");
 		    enemyCharacterHp = $(this).attr("character-hp");
 		    enemyCharacterCounter = $(this).attr("character-counter");
+		    enemyCharacterVar = $(this).attr("character-var");
+
 
 		    enemyCharacterPicked = true;
 
 		    $(".character").each(function () {
 		       	if ($(this).attr("character-name") != userCharacterName && $(this).attr("character-name") != enemyCharacterName) {
 		        	$("#enemiesLeft").append(this);
+		     
+
+		        	//trying to change the color of the enemies left to red in next 3 lines, but not working
+		        	// if ($("#" + userCharacterVar) <= 0) {
+		        	// 	$(".character").css("background-color", "red");
+		        	// }
 		        }
 
 		    });
@@ -135,27 +185,72 @@ function createButton() {
 
 	});
 
+	//attack button click
 	$("#attackButton").on("click", function() {
 
+		//only works if both characters picked
 		if (enemyCharacterPicked === true && userCharacterPicked === true) {
 			
+			//attack funtion
 			function attack() {
 				userCharacterHp -= enemyCharacterCounter;
 				enemyCharacterHp -= userCharacterAttack;
-				userCharacterAttack *= 2;
-
-
+	
 			}
 
 			attack();
 
+
+			//alerts to check values are adding correctly
 			alert(userCharacterName + " attacked " + enemyCharacterName + " for " + userCharacterAttack + " damage.");
 			alert(userCharacterName + " took " + enemyCharacterCounter + " damage from " + enemyCharacterName);
+			alert(userCharacterName + ": " + userCharacterHp + " ; " + enemyCharacterName + ": " + enemyCharacterHp);
 
+			userCharacterAttack *= 2;
+
+
+
+			//to change the hp values on the page to update to match above values
+			$("#" + userCharacterVar).text(userCharacterHp);
+
+			$("#" + enemyCharacterVar).text(enemyCharacterHp);
+
+
+
+			//if enemy dies
+			if (enemyCharacterHp <=0) {
+				alert("enemy dies");
+
+				//want to make the enemy who died button hidden (or maybe unclickable and change css to have X through it). 
+				//maybe add a class to the button div that can be styled to hide?
+					//then if all are hidden, say win game and reset
+
+				//$().addClass("dead")
+
+
+
+				//allows me to pick a new enemy and moves the old enemy back up
+				enemyCharacterPicked = false;
+
+
+			}
+
+			//if character dies
 			if (userCharacterHp <= 0) {
 				alert("You lost");
+
+				//game reset
+
+
+				//this almost works, but I am unable to click a new character on the second round for some reason...?
+
+				reset();
+				createButton();
+
+
 			}
 	
+
 		} else {
 			alert("Please pick your character and enemy first")
 		}
@@ -163,14 +258,13 @@ function createButton() {
 
     });
 
-		    console.log(userCharacterName);
-		    console.log(userCharacterHp);
-		    console.log(userCharacterAttack);
+
+
+
 
 
 });
 
-//Attack button click 
 
 
 
